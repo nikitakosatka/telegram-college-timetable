8af35dd8e11e4101cabf982d7357b3755160d964
nikitakosatka <nikitakosatka@yandex.ru>
2021-01-15 01:09:40 +0500

    Фиксы

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 import telebot
-from datetime import datetime
 
 from key import token
 from excel_parser import get_consultation_info
@@ -17,30 +16,9 @@ def send_welcome(message):
 @bot.message_handler(content_types=['text'])
 def get_indicated_time(message):
     try:
-        text = get_consultation_info(message.text)
+        surname = message.text[0].upper() + message.text[1:].lower()
+        text = get_consultation_info(surname)
         bot.send_message(message.chat.id, text, parse_mode="Markdown")
 
-    except ValueError:
-        bot.send_message(message.chat.id,
-                         "Ошибка, введите время в виде hh:mm",
-                         parse_mode="Markdown")
-
-
-def send_notice(chat_id, was_sent):
-    if not was_sent:
-        if datetime.today().isocalendar()[1] + 1 % 2 == 0:
-            is_odd = False
-        else:
-            is_odd = True
-
-        text = get_consultation_info(datetime.today().weekday() + 1, is_odd)
-        bot.send_message(chat_id, text, parse_mode="Markdown")
-
-
-def is_correct_time(time):
-    new_time = time.split(':')
-    if len(new_time) == 2:
-        if int(new_time[0]) in range(0, 24) and int(new_time[1]) in range(0, 60):
-            return True
-
-    return False
+    except KeyError:
+        bot.send_message(message.chat.id, "Введите фамилию преподавателя", parse_mode="Markdown")
